a0789737a6eacb7957093728ba25ff6673ecee22
Mariusz Gronczewski <xani666@gmail.com>
2015-07-29 17:26:07 +0200

    haproxy tests

--- cmd_test.go
+++ cmd_test.go
@@ -9,9 +9,25 @@ var testConn Conn
 
 func TestACL(t *testing.T) {
 	// fixme generate tmpfile unix socket
-	c := NewConnection("/tmp/file")
-	Convey("Add ACL", t, func() {
-		err := c.AddACL("test", "acl")
+	err := runTestHaproxy()
+	defer stopTestHaproxy()
+	Convey("Start test haproxy", t, func() {
 		So(err, ShouldEqual, nil)
 	})
+	c := NewConnection("tmp/haproxy.sock")
+	Convey("List ACL", t, func() {
+		out, err := c.RunCmd("show acl")
+		So(err, ShouldEqual, nil)
+		So(out[0], ShouldContainSubstring, "# id") // header
+	})
+	Convey("Add ACL", t, func() {
+		err := c.AddACL("t-data/blacklist.lst", "/bad/test1")
+		So(err, ShouldEqual, nil)
+	})
+	Convey("List ACL", t, func() {
+		out, err := c.RunCmd("show acl")
+		So(err, ShouldEqual, nil)
+		So(out[1], ShouldContainSubstring, `blacklist.lst`)
+	})
+	defer stopTestHaproxy()
 }
